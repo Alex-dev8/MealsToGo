e4774c5c7ba89143f31b0bc3863e50ae0a40b413
Alex-dev8 <cansal89@gmail.com>
2022-09-09 21:59:33 +0900

    Added searchbar and dependencies

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import { StatusBar } from "react-native";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
+import {Searchbar} from 'react-native-paper';
 
 export default function App() {
-    return (
-        <>
-         <SafeAreaView style={{flex: 1}}>
-         <View style={{ padding: 16, backgroundColor: 'green'}}>
-            <Text>Search</Text>
-         </View>
-         <View style={{ flex: 1, padding: 16, backgroundColor: 'blue'}}>
-            <Text>List</Text>
-         </View>
-        </SafeAreaView>
-        <StatusBar style='auto' />
-        </>
-    );
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
 });
