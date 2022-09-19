576a8a261a842563b761a73b493ee8873e97b296
Alex-dev8 <cansal89@gmail.com>
2022-09-19 17:38:45 +0900

    Completed map screen with detail view. Added custom callout cards.

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,16 +1,17 @@
 import React, {useContext, useEffect, useState} from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker, Callout} from 'react-native-maps';
 import {Search} from '../components/search.component';
 import styled from 'styled-components';
 import {LocationContext} from '../../../services/location/location.context';
 import {RestaurantsContext} from '../../../services/restaurants/restaurants.context';
+import {MapCallout} from '../components/map-callout.component';
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({navigation}) => {
   const {location} = useContext(LocationContext);
   const {restaurants} = useContext(RestaurantsContext);
   const [latDelta, setLatDelta] = useState(0);
@@ -34,7 +35,22 @@ export const MapScreen = () => {
           longitudeDelta: 0.02,
         }}>
         {restaurants.map(restaurant => {
-          return null;
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}>
+              <Callout
+                onPress={() =>
+                  navigation.navigate('RestaurantDetail', {restaurant})
+                }>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
+          );
         })}
       </Map>
     </>
