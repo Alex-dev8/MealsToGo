5252612f5ea3591e6a464f4db3524cf77248371a
Alex-dev8 <cansal89@gmail.com>
2022-09-23 00:22:47 +0700

    Account navigation

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthenticationContext} from '../../services/authentication/authentication.context';
 import {AppNavigator} from './app.navigator';
+import {NavigationContainer} from '@react-navigation/native';
+import {AccountNavigator} from './account.navigator';
 
+// We are taking the prop from AuthenticationContext, isAuthenticated. If it's true, we load the app, otherwise we load the account navigator for registration and login.
 export const Navigation = () => {
-  return <AppNavigator />;
+  const {isAuthenticated} = useContext(AuthenticationContext);
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
